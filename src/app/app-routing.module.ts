46f8c7b237fc210f492e9d87079c67d4613396b6
Bruno Cassanti <muniz.brunno@gmail.com>
2023-10-09 09:21:23 -0300

    feat(#35): routes were forwarded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,48 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './../components/nav-bar/nav-bar.component';
+import { HomeComponent } from 'src/components/home/home.component';
+import { ServicesComponent } from 'src/components/services/services.component';
+import { PortifolioComponent } from 'src/components/portifolio/portifolio.component';
+import { ContactComponent } from 'src/components/contact/contact.component';
+import { AboultUsComponent } from 'src/components/aboult-us/aboult-us.component';
+import { BlogComponent } from 'src/components/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'portifolio',
+    component: PortifolioComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'aboult-us',
+    component: AboultUsComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  }
+];
 
 @NgModule({
+  declarations: [], 
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+// app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from '../components/nav-bar/nav-bar.component'; // Importe o componente NavBarComponent aqui
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
