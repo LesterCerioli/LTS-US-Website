7bfa0085e567c8636546ec15372cdf35e98ba3e0
Luiz Guilherme Reis <luixguilherme@hotmail.com>
2023-10-11 13:51:51 -0300

    feat(#9): Maintenance of conflicts

--- src/components/services/educational-services/educational-services.component.ts
+++ src/components/services/educational-services/educational-services.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-educational-services',
   templateUrl: './educational-services.component.html',
-  styleUrls: ['./educational-services.component.css']
+  styleUrls: ['./educational-services.component.css'],
 })
-export class EducationalServicesComponent {
-
-}
+export class EducationalServicesComponent {}
