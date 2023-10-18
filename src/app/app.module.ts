835813f53cf669b890bdb1c2c91c04423b8c87a2
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-10-18 10:02:03 -0300

    feat(#3): fixing conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from 'src/components/home/home.component';
-import { NavBarComponent } from '../components/nav-bar/nav-bar.component';
+import { NavbarComponent } from 'src/components/nav-bar/nav-bar.component';
 import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { BlogComponent } from 'src/components/blog/blog.component';
 import { ContactComponent } from 'src/components/contact/contact.component';
@@ -21,6 +23,9 @@ import { FoodServicesComponent } from 'src/components/services/food-services/foo
 import { TitleComponent } from 'src/components/services/food-services/title/title.component';
 import { SecondSectionComponent } from 'src/components/services/food-services/second-section/second-section.component';
 import { ThirdSectionComponent } from 'src/components/services/food-services/third-section/third-section.component';
+import { MobileDevelopmentComponent } from 'src/components/services/mobile-development/mobile-development.component';
+import { MobileDevelopmentUpperSectionComponent } from 'src/components/services/mobile-development/mobile-development-upper-section/mobile-development-upper-section.component';
+import { MobileDevelopmentBottomSectionComponent } from 'src/components/services/mobile-development/mobile-development-bottom-section/mobile-development-bottom-section.component';
 import { RealStateServicesComponent } from 'src/components/services/real-state-services/real-state-services.component';
 import { BottomSectionStateComponent } from 'src/components/services/real-state-services/bottom-section-state/bottom-section-state.component';
 import { UpperSectionStateComponent } from 'src/components/services/real-state-services/upper-section-state/upper-section-state.component';
@@ -28,7 +33,7 @@ import { UpperSectionStateComponent } from 'src/components/services/real-state-s
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
+    NavbarComponent,
     AboutUsComponent,
     HomeComponent,
     BlogComponent,
@@ -47,13 +52,21 @@ import { UpperSectionStateComponent } from 'src/components/services/real-state-s
     TitleComponent,
     SecondSectionComponent,
     ThirdSectionComponent,
+    MobileDevelopmentComponent,
+    MobileDevelopmentUpperSectionComponent,
+    MobileDevelopmentBottomSectionComponent,
     RealStateServicesComponent,
     BottomSectionStateComponent,
     UpperSectionStateComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
 
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(library: FaIconLibrary){
+    library.addIcons(faGithub, faLinkedin)
+  }
+}
