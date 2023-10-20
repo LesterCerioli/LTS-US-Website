25008b459d347d695376a9222022fcd3e6e6a6c7
Bruno Cassanti <muniz.brunno@gmail.com>
2023-10-20 17:16:16 -0300

    feature(#routeBlog): blog routes

---
+++ src/components/blog/posts-component/cloudsuite/cloudsuite.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cloudsuite',
+  templateUrl: './cloudsuite.component.html',
+  styleUrls: ['./cloudsuite.component.css']
+})
+export class CloudsuiteComponent {
+
+}
